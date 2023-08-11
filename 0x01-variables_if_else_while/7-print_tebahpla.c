df44d0d89cf0f779ccb602521fb09aa60f61eee6
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-12 02:54:49 +0300

    updated

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : alphabet
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
