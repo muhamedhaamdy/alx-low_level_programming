ae4756984e0533dd7fdde03462302e85dc9f6f53
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-12 07:16:22 +0300

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,24 @@
 */
 int main(void)
 {
-	for (int i = 0; i < 100; i++)
+	int i = 0;
+
+	while (i < 100)
 	{
 		int a = i % 10, b = i / 10;
 
 		if (b >= a)
+		{
+			i++;
 			continue;
+		}
 		putchar(b + '0');
 		putchar(a + '0');
 		if (i == 89)
 			break;
 		putchar(',');
 		putchar(' ');
+		i++;
 	}
 	return (0);
 }
