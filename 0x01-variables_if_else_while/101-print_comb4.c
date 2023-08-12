2bd2e55d4ee5d8dffd1a8948b53c7cac4adb1be8
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-12 07:34:22 +0300

    updated

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : alphabet
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 1000)
+	{
+		int a = i % 10, b = i % 100 - a, c = i / 100 ;
+
+		if (c < b && c < a && b < a)
+		{
+			putchar(c + '0');
+			putchar(b + '0');
+			putchar(a + '0');
+			if (i == 789)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
