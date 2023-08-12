8c61027616185878ca41e21026a86f9c9064aebc
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-12 03:03:28 +0300

    updated

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : alphabet
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("0123456789abcdef\n");
+	return (0);
+}
