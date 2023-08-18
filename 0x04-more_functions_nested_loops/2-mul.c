5326006cf911c07f4deaf99ed25526b19e23389e
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-18 08:22:17 +0300

    updated

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code.
  * mul - function that multipliestwo numbers
  * @a: first num
  * @b: second num
- * Description: function  using builtin bunction isupper
- *
  * Return: Always 0.
  */
 int mul(int a, int b)
