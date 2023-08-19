320bad4de0c41da5f6ea0bc3f6f74eb3a3f46c50
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-19 23:40:32 +0300

    upping

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - it specifiy whether this char is letter or not
+ *
+ * @n : is the char that we shall determine
+ *
+ * Return: always return  0
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = -1 * n;
+	_putchar(n % 10 + '0');
+	return (n % 10);
+}
