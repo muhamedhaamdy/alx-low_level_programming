458f01f8a7950bd60d19ef549e4311e946f5a837
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-20 02:46:00 +0300

    upping

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,25 +3,34 @@
 /**
  * print_to_98- print from n to 98
  *
+ * print - all of this is shit
+ *
  * @n : strarting point
  *
  * Return: always return  0
  */
+void print(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n / 10)
+		print(n / 10);
+	_putchar(n % 10 + '0');
+}
 void print_to_98(int n)
 {
 	do {
-		int tmp = n;
-
-		while (tmp)
-		{
-			_putchar(tmp % 10 + '0');
-			tmp /= 10;
-		}
+		pirnt(n);
 		_putchar(',');
 		_putchar(' ');
 		if (n > 98)
 			n--;
-		else
+		else if (n <  98)
 			n++;
+		else
+			break;
 	} while (n != 98);
 }
