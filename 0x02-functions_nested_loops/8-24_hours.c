6a22fed61077a19c7ed33044900116f0aa151a3c
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-20 00:07:18 +0300

    upping

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,14 +21,14 @@ void jack_bauer(void)
 					_putchar(':');
 					_putchar(m1 + '0');
 					_putchar(m2 + '0');
-					_putchar('\n');
 					if (h1 == 2 && h2 == 3 && m1 == 5 && m2 == 9)
 						goto out;
+					_putchar('\n');
 				}
 			}
 		}
 	}
 out:
-	_putchar();
+	_putchar('\n');
 }
 
