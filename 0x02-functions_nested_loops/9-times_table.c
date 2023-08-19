51e10c1fb64e39aefe38e2d375d4d27bd8a43bf2
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-20 00:58:17 +0300

    upping

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,18 +1,27 @@
 #include "main.h"
 
 /**
- * times_table - pirnt time table of 9 
+ * times_table - pirnt time table of 9
  *
  * Return: always return  0
 */
 void times_table(void)
 {
 	int i = 0, j;
-	while (i <=9)
+
+	while (i <= 9)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(i*j + '0');
+			int mul = i * j;
+
+			if (mul  < 10)
+				_putchar(i * j + '0');
+			else
+			{
+				_putchar(mul / 10 + '0');
+				_putchar(mul % 10 + '0');
+			}
 			if (j == 9)
 				_putchar('\n');
 			else
