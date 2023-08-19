14392286aa6102d319b940bccd0f6783424f38df
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-20 02:04:24 +0300

    upping

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98- print from n to 98
+ *
+ * @n : strarting point
+ *
+ * Return: always return  0
+ */
+void print_to_98(int n)
+{
+	do {
+		int tmp = n;
+
+		while (tmp)
+		{
+			_putchar(tmp % 10 + '0');
+			tmp /= 10;
+		}
+		_putcahr(',');
+		_putchar(' ');
+		if (n > 98)
+			n--;
+		else
+			n++;
+	} while (n != 98)
+}
