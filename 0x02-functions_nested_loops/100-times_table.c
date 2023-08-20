08d66e1713b872322d3eeb3d2487d55a267c8ce4
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-20 13:01:13 +0300

    upping

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_table - pirnt time table of 9
+ *
+ * Return: always return  0
+*/
+void print_times_table(int n)
+{
+	int i = 0, j;
+
+	while (i <= n)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int mul = i * j;
+
+			if (mul  < 10)
+				_putchar(i * j + '0');
+			else
+			{
+				_putchar(mul / 10 + '0');
+				_putchar(mul % 10 + '0');
+			}
+			if (j == n)
+				_putchar('\n');
+			else
+			{
+				_putchar(',');
+				if ((j + 1) * i <= 100)
+					_putchar(' ');
+				else if((j +1) * i <=10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else 
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+		}
+		i++;
+	}
+
+}
