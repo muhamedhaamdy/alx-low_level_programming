4ae8246596a82bf7bc45506c639ad01704746162
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-20 23:48:12 +0300

    upping

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
-
 /**
- * main - main function
- *
- * description : print all numbers by 1024 that divisble by 3 and 5
- *
- * return : always 0
- * */
-
+*main - prints count of multiples
+*of 3 or 5 below 1024
+*Return: return 0
+*/
 int main(void)
 {
-	int i, sum = 0; 
+	int i, sum = 0;
 
 	for (i = 1; i < 1024; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	}
 	printf("%d\n", sum);
