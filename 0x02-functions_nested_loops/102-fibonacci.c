9d6aaa6b1e23213681492858266a5a3dc0b8f619
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-21 00:23:28 +0300

    upping

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	long long i, a = 1, b = 2, tmp;
 
-	printf("%lli, %lli, ", a, b);
+	printf("%lld, %lld, ", a, b);
 	for (i = 0; i < 48; i++)
 	{
 		tmp = a + b;
 		a = b;
 		b = tmp;
 		if (i == 47)
-			printf("%lli", b);
-		printf("%lli, ", b);
+			printf("%lld", b);
+		printf("%lld, ", b);
 	}
 	putchar('\n');
 	return (0);
