1bd3d22acd804556e9f7ba5f9fbdce12d57b226f
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-20 12:00:35 +0300

    upping

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,32 +1,31 @@
 #include "main.h"
 
 /**
- * print_to_98- print from n to 98
- * print - recursive function that prins the full number
- *
- * Description : print the number
- *
+ * cout- recursive function that prins the full number
  * @n : strarting point
- * @k : pass to print function
- *
- * Return: always return  0
  */
-void print(int k)
+void cout(int n)
 {
-	if (k < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		k *= -1;
+		n *= -1;
 	}
-	if (k / 10)
-		print(k / 10);
-	_putchar(k % 10 + '0');
+	if (n / 10)
+		print(n / 10);
+	_putchar(n % 10 + '0');
 }
+/**
+ * print_to_98 - print numbers from n to 98
+ *
+ * @n: starting point
+ *
+ */
 void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		print(n);
+		cout(n);
 		if (n > 98)
 			n--;
 		else if (n <  98)
