aa7d4518fb9cfb848b4e9428dc6c93299582a3ee
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-22 02:13:55 +0300

    upping

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+include"main.c"
+
+/**
+ * reset_to_98 - function that reset the value to 98
+ *
+ * @n : a pionter that should be reseted
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+int _putchar(char);
+void reset_to_98(int *n);
+
+
+
+#endif /* MAIN_H */
