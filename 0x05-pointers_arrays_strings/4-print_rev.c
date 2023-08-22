3da372d587db223a8f15b7ce81d5bcf5923a72ad
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-22 05:08:52 +0300

    upping

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,23 @@
 #include "main.h"
+/**
+ * _strlen - function calculate the lengnth of string
+ *
+ * @s : the string
+ *
+ * Return: always return the lentgth of the string
+ */
+int _strlen(char *s)
+{
+	char i = *s;
+	int c = 0;
 
+	while (i != '\0')
+	{
+		c++;
+		i = *(s + c);
+	}
+	return (c);
+}
 /**
  * print_rev - function prints the full stirng
  *
