e39a267773ad6bee390a10e54c7e1f19b292b03a
Mohamed Hamdy <mmmhamdyyy@gmail.com>
2023-08-21 18:34:31 -0700

    upping

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cout- recursive function that prins the full number
+ * @n : strarting point
+ */
+void cout(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n / 10)
+		cout(n / 10);
+	_putchar(n % 10 + '0');
+}
+/**
+ * print_array - print array elements
+ *
+ * @a : array pointer
+ * @n : array size
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = a; i < a + n; i++)
+	{
+		cout(*i);
+		if (i == a + n - 1)
+			break;
+		_putchar(',');
+		_putcahr(' ');
+	}
+	_putchar('\n');
+}
