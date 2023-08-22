0f0adb8103d4e9363fea446bd7474e1acb692c03
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-08-22 05:53:37 +0300

    upping

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strlen - function calculate the lengnth of string
+ *
+ * @s : the string
+ *
+ * Return: always return the lentgth of the string
+ */
+int _strlen(char *s)
+{
+	char i = *s;
+	int c = 0;
+
+	while (i != '\0')
+	{
+		c++;
+		i = *(s + c);
+	}
+	return (c);
+}
+/**
+ * puts_half - function prints the half of the string
+ *
+ * @str : the string
+ */
+void puts_half(char *str)
+{
+	int len = _strlen(s) - 1;
+	int half = len / 2;
+	char ch = *(str + half);
+
+	while (ch != '\0')
+	{
+		_putchar(ch);
+		half++;
+		ch = (str + half);
+	}
+	_putchar('\n');
+}
