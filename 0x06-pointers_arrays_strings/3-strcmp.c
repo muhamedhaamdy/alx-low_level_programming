f7dd5eb4dd494991d9f477de03a5a463531841d8
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-25 12:07:31 -0400

    upping

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _strcmp - function that compare two strings
- *
- * @s1 : first string
- * @s2 : second string
- * Return: return which of two strings are bigger
-*/
+ * _strcmp - compare 2 strings.
+ * @s1: string to be compared.
+ * @s2: string for comparation.
+ * Return: Difference.
+ */
 int _strcmp(char *s1, char *s2)
 {
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 	if (*s1 == *s2)
+	{
 		return (0);
+	}
 	else
-		return (*s1 - *s2);
+	{
+	return (*s1 - *s2);
+	}
 }
