f4ca5f6ecd5c0a7bfed309442763571c982e4f71
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-25 08:48:24 -0400

    upping

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - function that copy two strings
+ *
+ * @dest : first string
+ * @src : second string
+ * @n : number of bits
+ * Return: return a pointer to the first string after concatenation
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *i = dest, *j = src;
+
+	while (*j != '\0' && n--)
+	{
+		*i = *j;
+		i++;
+		j++;
+	}
+	return (dest);
+}
