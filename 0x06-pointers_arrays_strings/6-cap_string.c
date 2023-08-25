ac3981305b6be1ffda7f18785aa2a24d47714eff
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-25 13:09:07 -0400

    upping

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
- * is_alpha - determine if it is alphabit or not
+ * not_alpha - determine if it is alphabit or not
  * @ch : the character
  * Return: 1 or 0
 */
-int is_alpha(char ch)
+int not_alpha(char ch)
 {
-	if (ch == ',' || ch == ';' || ch == '.' || ch == '!' || ch == '?' || ch == '"' ||ch == '(' || ch == ')' || ch == '{' || ch == '}' || ch == ' ' || ch == '\n' || ch == '\t')
+	char *s = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'}
+
+	for (int i = 0; i < 13; i++)
+	{
+		if (ch == s[i])
+			return (1)
+	}
 		return (0);
-	else
-		return (1);
 }
 /**
  * cap_string - function that turns all lowercase chars to uppercase
@@ -22,7 +26,7 @@ char *cap_string(char *ch)
 
 	for (; ch[i] != '\0'; i++)
 	{
-		if (!is_alpha(ch[i - 1])  && (ch[i] >= 'a' && ch[i] <= 'z'))
+		if (not_alpha(ch[i - 1])  && (ch[i] >= 'a' && ch[i] <= 'z'))
 			ch[i] -= 32;
 	}
 	return (ch);
