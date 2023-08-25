6592601079f79244ea48f39cd8eda2468d663679
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-25 09:49:10 -0400

    upping

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int sum1 = 0, sum2 = 0, i = 0;
 
-	while (s1[i] != '\0')
+	for ( ; s1[i] != '\0'; i++)
 		sum1 += s1[i];
-	i = 0'
-	while(s2[i] != '\0')
+	for (i = 0; s2[i] != '\0'; i++)
 		sum2 += s2[i];
-	if(sum1 == sum2)
+	if (sum1 == sum2)
 		return (0);
-	else if(sum1 > sum2)
+	else if (sum1 > sum2)
 		return (15);
 	else
 		return (-15);
