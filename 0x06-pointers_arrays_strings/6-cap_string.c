23d4ec35d697e0570fed895aa45e4c1a910e012d
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-25 18:32:11 -0400

    upping

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,32 @@
 #include "main.h"
 /**
  * cap_string - function that turns all lowercase chars to uppercase
- * @ch : string
+ * @entry: string
  * Return: return string after editing.
 */
-char *cap_string(char *s)
+char *cap_string(char *entry)
 {
-	int i, j;
-	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int conversion, index, count;
 
-	i = 0;
-	while (*(s + i) != '\0')
+	char chars[] = {' ', ',', ';', '.', '!',
+			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
+	conversion = 32;
+
+	for (index = 0; entry[index] != '\0'; index++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		if (entry[index] >= 'index' && entry[index] <= 'z')
 		{
-			if (i == 0)
+			entry[index] =  entry[index] - conversion;
+		}
+		conversion = 0;
+		for (count = 0; chars[count] != '\0'; count++)
+		{
+			if (chars[count] == entry[index])
 			{
-				*(s + i) = *(s + i) - 32;
-			}
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-					{
-						*(s + i) = *(s + i) - 32;
-					}
-				}
+				conversion = 32;
+				break;
 			}
 		}
-	i++;
 	}
-	return (s);
+	return (entry);
 }
