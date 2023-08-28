ba69bd9fd1266a4d7192405acfc5cf90adc0ee5f
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-28 09:55:33 -0400

    upping

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,7 +12,7 @@ char *_strpbrk(char *s, char *accept)
 	int i, j;
 	char *loc = '\0';
 
-	for (i = 0 ; accept[i] >= '\0'; i++)
+	for (i = 0 ; accept[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] >= '\0'; j++)
 		{
