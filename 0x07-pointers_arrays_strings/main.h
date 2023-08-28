0c1c9a8d92883e8119ca4ab9062f1587cee38f91
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-27 23:59:40 -0400

    upping

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+
+
+
+
+
+#endif /* MAIN_H */
