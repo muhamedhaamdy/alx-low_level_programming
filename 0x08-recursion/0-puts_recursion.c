6c61a5cff3cac68322719ecd9b7a7f4b2375e459
Mohamed Hamdy <mmmhamdyyy@gmail.com>
2023-08-30 03:47:57 -0700

    upping

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string in recusive way
+ *
+ * @s - the string
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+		retrun;
+	_putchar(s[i]);
+	_puts_recusion(s + i + 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+int _putchar(char);
+void _puts_recursion(char *s);
+
+
+#endif /* MAIN_H */
