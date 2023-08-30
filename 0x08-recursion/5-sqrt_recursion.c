e21b4667a4797d4d016b30bde8717a5c9b7d34ac
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-30 16:22:55 -0400

    upping

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,9 +9,9 @@ int _sqrt_help(int n, int i)
 {
 	if (i * i == n)
 		return (i);
-	else if (i == 0)
+	else if (i == n)
 		return (-1);
-	return (_sqrt_help(n, i - 1));
+	return (_sqrt_help(n, i + 1));
 }
 /**
  * _sqrt_recursion - calculate the square root of a number
@@ -22,5 +22,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt_help(n, n / 2));
+	return (_sqrt_help(n, 1));
 }
