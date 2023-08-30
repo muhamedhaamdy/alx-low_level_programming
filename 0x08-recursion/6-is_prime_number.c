24dee44dea75e7e534a151c84f5cc2198db94c1b
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-30 16:37:23 -0400

    upping

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * is_prime_help - help the first function
+ * @n : first number
+ * @i : second number
+ * Return: 1 or 0
+*/
+int is_prime_help(int n, int i)
+{
+	if (n == i)
+		return (1);
+	if (n % i)
+		return (0);
+	return (is_prime_help(n, i + 1));
+}
+/**
+ * is_prime_number - calculate the power of a number
+ * @n: the second number
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (is_prime_help(n, 2));
+}
