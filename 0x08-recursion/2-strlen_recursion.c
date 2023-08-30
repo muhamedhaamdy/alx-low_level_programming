2e4c175b1717caaff6f4840c7289381be12b5f7f
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-30 09:40:45 -0400

    upping

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,22 +1,18 @@
 #include<stdio.h>
 /**
- * _print_rev_recursion - print a string in recusive way
- *
+ * _strlen_recursion - get the length of the string
+ * @s : the string
+ * Return: string length
  */
 int _strlen_recursion(char *s)
 {
 	int i = 0;
 
 	if (s[i] == '\0')
-		return (i);
-	i++;
-	_strlen_recursion(s + i);
-}
-int main(void)
-{
-    int n;
-
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
-    return (0);
+		return (0);
+	else 
+	{
+		i = _strlen_recursion(s + i + 1);
+		return (i + 1);
+	}
 }
