2778889dd3526c5ed4358f2439db28e614225d04
Mohamed Hamdy <mmmhamdyyy@gmail.com>
2023-08-30 03:59:11 -0700

    upping

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,17 @@
 /**
  * _puts_recursion - print a string in recusive way
  *
- * @s - the string
+ * @s : the string
  */
 void _puts_recursion(char *s)
 {
 	int i = 0;
 
 	if (s[i] == '\0')
+	{
+		_putchar('\n');	
 		return;
+	}
 	_putchar(s[i]);
 	i++;
 	_puts_recursion(s + i);
