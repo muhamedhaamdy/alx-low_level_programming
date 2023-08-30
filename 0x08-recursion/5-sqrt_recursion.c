8399bbffb7b96d0f5ac5f8ef88d50ebb38887e1b
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-30 15:51:13 -0400

    upping

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,26 @@
 #include<stdio.h>
 /**
+ * _sqrt_help - this help the fiest funtion
+ * @n : the first number
+ * @i : the second number
+ * Return: the square root if exist
+*/
+int _sqrt_help(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i == 0)
+		return (-1);
+	return (_sqrt_help(n, i - 1));
+}
+/**
  * _sqrt_recursion - calculate the square root of a number
  * @n : the number
  * Return: the square root if exist
  */
 int _sqrt_recursion(int n)
 {
-	int tmp = n;
-
-	if (n * n == tmp)
-		return (n);
-	if (n == 0)
+	if (n < 0)
 		return (-1);
-	return (_sqrt_recursion(n - 1));
+	return (_sqrt_help(n, n));
 }
