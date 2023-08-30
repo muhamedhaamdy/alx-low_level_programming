26ea960e8a54bb4078ba4a9f66a40c4802764d6f
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-30 09:14:12 -0400

    upping

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,5 @@
 #include "main.h"
 /**
- * len - get the length of the string
- *
- * @s : the string
- * Return : the length of the string
-*/
-int len(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-/**
  * _print_rev_recursion - print a string in recusive way
  *
  * @s : the string
