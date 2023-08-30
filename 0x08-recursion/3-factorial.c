214936ae13aee4c9265ff60f5c502383425df396
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-08-30 10:04:29 -0400

    upping

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * factorial - calculate the factorial of a number
+ * @n : the number
+ * Return: the factorial of the number
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	return (n * (n - 1));
+}
