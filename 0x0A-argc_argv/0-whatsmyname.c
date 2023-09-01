72973665046238606f3a7a2d309adcf1dbd422b2
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-09-01 09:12:51 -0400

    upping

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 /**
  * main - main function
  *
- * @s : the string
- * Return : always 0
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
-	return (0);	
+	return (0);
 }
