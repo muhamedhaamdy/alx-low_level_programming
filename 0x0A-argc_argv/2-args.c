922127c04a7cf1f94049ca2e48472070d57dcc4f
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-09-01 09:27:06 -0400

    upping

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for(;i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
