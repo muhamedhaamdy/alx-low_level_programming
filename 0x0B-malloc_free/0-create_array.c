f480e5e0a08ca10fad17dd401aa9c4e6f38aeea2
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-09-04 17:30:59 -0400

    upping

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,9 @@ char *create_array(unsigned int size, char c)
 	char *arr = malloc(size);
 	unsigned int i;
 
+	if (arr == '\0' || size == 0)
+		return ('\0');
+		printf("");
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 	return (arr);
