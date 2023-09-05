f51aa0d545756e8fbc09fec3415499b2d21d271a
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-09-05 13:58:20 -0400

    upping

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,21 +13,21 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	arr = calloc(sizeof(int), width);
+	arr = malloc(sizeof(int) * height);
 	if (arr == NULL)
 		return (NULL);
 	for (; i < width;  i++)
 	{
-		arr[i] =  calloc(sizeof(int), height);
+		arr[i] =  malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
-			return (NULL);
-	}
-	for (i = 0; i < width;  i++)
-	{
-		for (j = 0; j < height; j++)
 		{
-			arr[i][j] = 0;
+			while (i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
 	}
 	return (arr);
 }
