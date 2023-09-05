915929f54ad26177eaf1b18edf5db6c83d22f892
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-09-04 20:34:17 -0400

    upping

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - copy a string
+ *
+ * @str : the stirng
+ * Return: the string
+ */
+char *_strdup(char *str)
+{
+	char *arr = malloc(sizeof(char));
+	unsigned int i = 0;
+
+	do {
+		if (arr == NULL)
+			return ('\0');
+		arr[i] = str[i];
+		i++;
+		arr = (char *)realloc(arr, i + 1);
+	} while (str[i] != '\0');
+	return (arr);
+}
