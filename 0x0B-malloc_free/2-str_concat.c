4c0e5e13e886074289f4732e123dac1e6c64a2f0
Mohamed Hamdy <mmhamdyyy@gmail.com>
2023-09-04 21:09:39 -0400

    upping

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,21 +14,21 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
+	while (s1[i] != NULL)
 		i++;
-	while (s2[j] != '\0')
+	while (s2[j] != NULL)
 		j++;
 	arr = (char *) malloc(i + j + 1);
 	i = 0;
 	if (arr == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
+	while (s1[i] != NULL)
 	{
 		arr[i] = s1[i];
 		i++;
 	}
 	j = 0;
-	while (s2[j] != '\0')
+	while (s2[j] != NULL)
 	{
 		arr[i] = s2[j];
 		i++;
