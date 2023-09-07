68de430925faba6fb811a2d489b92bca075bc2f0
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-08 01:08:15 +0300

    upping

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 /**
  * _calloc -  allocates memory for an array,
- * 
+ *
  * @nmemb : the size of this memory
  * @size : the size of bits
- * Return: address 
+ * Return: address
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	 void *arr;
 
-	arr = callock(size, nmemb);
+	arr = (void *)calloc(size, nmemb);
 	if (arr == NULL)
-		return (NUL)L;
+		return (NULL);
 	return (arr);
 }
