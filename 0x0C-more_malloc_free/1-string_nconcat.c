a3cbb5a7d09f868265e16698ce63bee95f325c69
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-08 00:35:16 +0300

    upping

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,14 +43,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (s);
 	}
 	else if (s2 == NULL)
-		s = malloc(sz1);
+		s = malloc(sz1 + 1);
 	else if (s1 == NULL)
-		s = malloc(min(sz2, n));
+		s = malloc(min(sz2, n) + 1);
 	else
-		s = malloc(sz1 + min(sz2, n));
+		s = malloc(sz1 + min(sz2, n) + 1);
 	for (; i < sz1; i++)
 		s[i] = s1[i];
 	for (; j < min(sz2, n); j++, i++)
 		s[i] = s2[j];
+	s[i] = '\0';
 	return (s);
 }
