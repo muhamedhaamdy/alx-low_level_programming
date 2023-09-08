7055044fd0540814613faf15e89a4a12812fab03
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-09 02:51:28 +0300

    upping

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * (max - min) + 1);
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < max - min + 1; i++, min++)
