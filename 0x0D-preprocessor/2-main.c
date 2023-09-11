58165fa2e238b397a25d610d57aca8365008b9e5
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-12 00:58:23 +0300

    kosom n3ma

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#ifndef __FILE__
+#define __FILE__
+#endif
+/**
+ * main - entry function
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
