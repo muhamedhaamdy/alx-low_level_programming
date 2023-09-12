343461b4d642ce7f9b5df35553f578ecc7bc10c6
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-12 04:30:05 +0300

    upping

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,8 @@
 #include "dog.h"
 /**
  * init_dog - initialize the struct
+ *
+ * @d : the address for the struct
  * @name : the name of dog
  * @age : the age of the dog
  * @owner : the owner of the dog
@@ -9,5 +11,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
-	d->owner - owner;
+	d->owner = owner;
 }
