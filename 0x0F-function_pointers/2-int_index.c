61de792807a718917d44701ccd7ebb1cf7fbc1e5
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-14 02:26:58 +0300

    upping

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - saerches for and integer
+ * @array : array for int
+ * @size : the size of the array
+ * @cmp : the function that compare the integer
+ * Return: the index of first appearance
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (!array || !cmp)
+		return;
+	int i = 0, indx;
+
+	for (; i < size; i++)
+	{
+		if (cmp[arr[i]])
+		{
+			indx = i;
+			break;
+		}
+	}
+}
