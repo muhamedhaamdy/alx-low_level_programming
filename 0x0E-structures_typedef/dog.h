35e7ce9beb935c90c558b517d1c682554bc8d009
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-13 06:20:40 +0300

    upping

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,11 @@
  *
  * Description: just a lone dog struct in a big kitty world
  */
-struct dog
+typedef struct dog
 {
 	char *name, *owner;
 	float age;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
