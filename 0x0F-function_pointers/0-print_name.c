55777b3a92286e061a3879c6b93a30c6e290daea
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-14 00:24:35 +0300

    upping

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - prints the name(stirng)
  * @name : the name(string)
- * @f : the function that prints the name(stirng)
+ * @f : the function that prints the name(string)
 */
 void print_name(char *name, void (*f)(char *))
 {
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - prints element in an indirect way
+ * @array : array for int
+ * @size : the size of the array
+ * @action : the function that prints the number
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (!action)
+		return;
+	for (; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
