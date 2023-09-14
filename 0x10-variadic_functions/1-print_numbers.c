8b6c2b8b00b1b985373977a7ccbfd69f2e9a2151
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-15 01:54:18 +0300

    upping

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,13 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ptr, n);
 	while (i < n)
 	{
-		if (i == n - 1)
-			printf("%d\n", va_arg(ptr, int));
-		else if (separator)
+		if (separator)
 			printf("%d%s", va_arg(ptr, int), separator);
 		else
 			printf("%d", va_arg(ptr, int));
 		i++;
 	}
+	printf("\n");
 	va_end(ptr);
 }
