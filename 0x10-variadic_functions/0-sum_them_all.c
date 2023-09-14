ec428b6598740abfcbfb1917fbd961a4ef64fb2d
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-15 00:27:14 +0300

    upping

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_function.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sum all numbers
+ * 
+ * @n : number of integers
+ * Return: summation
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0, i = 0;
+	va_list ptr;
+
+	va_start(ptr, n);
+	while (i < n)
+	{
+		sum += va_args(ptr, int);
+		i++;
+	}
+	va_end(ptr);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+#endif
