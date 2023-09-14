d708217633070a32171d2b9885a8b0cd9cbe684e
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-14 06:44:55 +0300

    upping

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 #include <stdlib.h>
-int _putchar(char);
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif /* MAIN_H */
+
+#endif
