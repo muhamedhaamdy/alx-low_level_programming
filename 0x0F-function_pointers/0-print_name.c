baea4ccbf6bbffdbaf9717cd5174d55e7559f352
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-14 03:42:08 +0300

    upping

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 /**
  * print_name - prints the name(stirng)
+ * @name : the name(string)
  * @f : the function that prints the name(string)
 */
 void print_name(char *name, void (*f)(char *))
