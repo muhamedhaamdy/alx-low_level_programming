3dfd240e56bfdedae493980ef55060fe61cf1b48
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-15 05:19:18 +0300

    upping

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,13 +2,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
+ * next_valid - check the next valid char
+ *
+ * @s : the stirng
+ * @i : the index
+ * Return: the index of the next valid
+*/
+int next_valid(char *s, int i)
+{
+	while (s[i])
+	{
+		if (s[i] == 'c' || s[i] == 'i' || s[i] == 's' || s[i] == 'f')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+/**
  * print_all - prints all strings
  *
  * @format : a string
 */
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i = 0;
 	va_list pt;
 	char *s;
 
@@ -16,21 +33,26 @@ void print_all(const char * const format, ...)
 	while (format[i])
 	{
 		switch(format[i])
-		case 'c':
-			printf(format[i+1]? "%c, ": "%c\n", va_arg(pt, char);
-			break;
-		case 'i':
-			printf(format[i+1]? "%d, ": "%d\n", va_arg(pt, int));
-			break;
-		case 's':
-			s = va_arg(pt, char *);
-			printf(format[i+1]? "%s, ": "%s\n", s? s : "(nil)"));
-			break;
-		case 'f':
-			printf(format[i+1]? "%f": "%f\n", va_arg(pt, double));
-			break;
-		default:
-			break;
+		{
+			case 'c':
+				printf("%c", va_arg(pt, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(pt, int));
+				break;
+			case 's':
+				s = va_arg(pt, char *);
+				printf("%s", s? s : "(nil)");
+				break;
+			case 'f':
+				printf("%f", va_arg(pt, double));
+				break;
+			default:
+				break;
+		}
+		i++;
+		if (next_valid(format, i))
+			pirntf(", ");
 	}
 	printf("\n");
 }
