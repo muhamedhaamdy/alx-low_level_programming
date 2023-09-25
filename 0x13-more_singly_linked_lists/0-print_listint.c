ea3efbb9dde6011548f9988fecaa0f3e938146cb
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-25 21:12:38 +0300

    upp

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_listint - print all the element in the list
+ * @h : the head of the list
+ * Return : */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
