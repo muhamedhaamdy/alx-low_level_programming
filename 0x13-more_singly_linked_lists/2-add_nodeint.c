a98304ddc577f39e005369b85478d7cdb289d30a
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-25 21:58:20 +0300

    hotfix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,11 +8,12 @@
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t node;
+	listint_t *node;
 	node = malloc(sizeof(listint_t));
 	if (!node)
 		return (NULL);
 	node->n = n;
 	node->next = *head;
 	*head = node;
+	return (node);
 }
