339ee1e6744e35724c81b6f97123de9aff5c53af
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-25 21:26:05 +0300

    hotfix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,14 +3,15 @@
 /**
  * print_listint - print all the element in the list
  * @h : the head of the list
- * Return : */
+ * Return: the number of elemeants
+*/
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
-		printf("%d\n",h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
