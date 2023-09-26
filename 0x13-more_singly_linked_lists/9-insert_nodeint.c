53a5f1226752f540aed2512b47db0b013952d083
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-27 01:13:54 +0300

    hotfix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	node = malloc(sizeof(listint_t));
-	if (!node)
+	if (!node || !head)
 		return (NULL);
 	node->n = n;
 	curr = *head;
