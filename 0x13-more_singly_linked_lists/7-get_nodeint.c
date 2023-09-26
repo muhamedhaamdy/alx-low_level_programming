f445a872d12fffd3542da0064b289c02f4fc7a8b
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-26 22:35:47 +0300

    hotfix

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get the node at specific index
+ * @index : the index
+ * Return: the node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int i = 0;
+
+	while (head)
+	{
+		if (i == head->n)
+			return (head);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
