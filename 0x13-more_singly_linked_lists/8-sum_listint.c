cb38abe1617ed5aaff91d0b862a5f8b7c3116884
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-27 00:05:00 +0300

    hotfix

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * sum_listint - get the sum of all list elements
+ * @head : the head of the list
+ * Return: the sum
+*/
+int sum_listint(listint_t *head){
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
