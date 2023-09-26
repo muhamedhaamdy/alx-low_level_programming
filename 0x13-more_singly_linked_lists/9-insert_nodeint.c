37882dc66ba703de86ca156bb41793675af94520
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-27 01:13:06 +0300

    hotfix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		else if (i == idx - 1)
 		{
-			noce->next = curr->next;
+			node->next = curr->next;
 			curr->next = node;
 			return (node);
 		}
