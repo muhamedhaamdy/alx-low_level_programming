be9cdc73592c0eeb36a94b0cfa97b0a8c9ff7097
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-29 03:36:43 +0300

    hotfix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+
 int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
