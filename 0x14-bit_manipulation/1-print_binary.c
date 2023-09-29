0e872f09392994a46697953fd6f7c177152a6cd9
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-29 03:02:31 +0300

    hotfix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,12 +7,22 @@
 */
 void print_binary(unsigned long int n)
 {
-	int size = sizeof(n) * 8, i, bit;
+	int size = sizeof(n) * 8, i, bit, first = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	for (i = size - 1; i >= 0; i++)
 	{
 		bit = (n >> i) & 1;
-		_putchar(bit + '0');
+		if (first == 0 && bit)
+		{
+			_putchar(bit + '0');
+			first = 1;
+		}
+		else if (first)
+			_putchar(bit + '0');
 	}
-	_putchar('\n');
 }
