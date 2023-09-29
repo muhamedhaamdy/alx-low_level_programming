8330c0fa59f5a5757de0ae9afeb922276ae1c899
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-29 04:33:59 +0300

    hotfix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int size = sizeof(n) * 8, i, bit, first = 0;
+	unsigned int size = sizeof(n) * 8, i, bit;
 	int tmp = i = size - 1;
 
 	for (; tmp >= 0; tmp--, i--)
