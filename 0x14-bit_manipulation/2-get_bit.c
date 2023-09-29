d85cecce37a432201620f18ca60812544960cb5a
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-09-29 04:00:49 +0300

    hotfix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * get_bit - get a bin at specific index
+ * get_bit - get a bit at specific index
  *
+ * @n : the number
  * @index: the index
  * Return: the bit
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int size = sizeof(n) * 8, i, bit, first = 0;
+	unsigned int size = sizeof(n) * 8, i, bit, first = 0;
+	int tmp = i = size - 1;
 
-	if (n == 0)
-	{
-		putchar('0');
-		return;
-	}
-	for (i = size - 1; i >= 0; i--)
+	for (; tmp >= 0; tmp--, i--)
 	{
 		bit = (n >> i) & 1;
 		if (!first && bit)
@@ -29,4 +26,5 @@ int get_bit(unsigned long int n, unsigned int index)
 		else if (!first && index == i)
 			return (-1);
 	}
+	return (-1);
 }
