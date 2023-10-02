198dba55573c1d596da9b6dca9e86699212b14d3
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-10-02 21:32:45 +0300

    hotfix

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "mian.h"
+
+/**
+ * read_textfile - read the text file
+ *
+ * @filename : the name of the file
+ * @letters : number of letter in the file
+ * Return: the acual number of letter that could read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *filePtr = fopen(filename, "r");
+	int i = 0, ch;
+
+	if (!filePtrf || !filename)
+		return (0);
+	while ((ch = fgetc(filePtr)) != EOF)
+	{
+		putchar(ch);
+		i++;
+	}
+	fclose(filePtr);
+	return (i);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+int _putchar(char);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
