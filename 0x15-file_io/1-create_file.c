92b9a1c4af659d9016791ca7c7bc02d11f19d9ce
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-10-03 01:02:32 +0300

    hotfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int filePtr = open(filename, O_RDWR | O_CREAT, 0644);
+	int filePtr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	size_t i = 0;
 	ssize_t bytes;
 
