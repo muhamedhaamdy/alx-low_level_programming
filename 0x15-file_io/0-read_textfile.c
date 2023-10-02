7d3c265a9465c2ebe60030c1c482a02724e2aac5
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-10-03 00:44:05 +0300

    hotfix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	while ((ch = fgetc(filePtr)) != EOF && i < letters)
 	{
-		putchar(ch);
+		_putchar(ch);
 		i++;
 	}
 	fclose(filePtr);
