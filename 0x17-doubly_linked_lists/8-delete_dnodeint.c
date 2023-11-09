b810bc383297146dd863c3244f0bd48c363241bc
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-10 01:36:59 +0200

    "hotfix"

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,46 +5,35 @@
  * @index : the index of the node
  * Return: 1 or -1
 */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *curr = *head;
+	dlistint_t *current = *head, *temp = NULL;
 	unsigned int i = 0;
 
-	if (!*head || !head)
+	if (*head == NULL)
 		return (-1);
-	if (!index)
+	if (index == 0)
 	{
-		curr = curr->next;
-		curr->prev = NULL;
-		free(*head);
-		*head = curr;
+		temp = *head;
+		*head = (*head)->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(temp);
 		return (1);
 	}
-	while (curr->next)
+	while (current)
 	{
-		if (i == index)
+		if (i == index - 1)
 		{
-			if (curr->next && curr->prev)
-			{
-				curr->prev->next = curr->next;
-				curr->next->prev = curr->prev;
-				free(curr);
-				return (1);
-			}
-			else if (!curr->next && !curr->prev)
-			{
-				*head = NULL;
-				return (1);
-			}
+			temp = current->next;
+			current->next = temp->next;
+			if (temp->next)
+				temp->next->prev = current;
+			free(temp);
+			return (1);
 		}
-		curr = curr->next;
+		current = current->next;
 		i++;
-	}
-	if (i == index)
-	{
-		curr->prev->next = NULL;
-		return (1);
 	}
 	return (-1);
 }
