e7acd2282a8a76bfde1d218a8b9e401f012cfca0
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-09 22:07:47 +0200

    "hotfix"

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,10 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	 dlistint_t *curr;
+	dlistint_t *curr;
 	int c = 0;
 
-	curr = h;
+	curr = (dlistint_t *) h;
 	while (curr)
 	{
 		printf("%d\n", curr->n);
