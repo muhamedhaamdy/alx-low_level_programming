36d3fb4ab5f1380c0dc6204bdc0d30cd10f540d3
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-09 20:12:36 +0200

    upping

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * print_dlistint - function that prints linkedlist
+ * print_dlistint - prints all the elements of a dlistint_t list
  *
- * @h : head of the linkedlist
- * reutrn : the size of the linked list
-*/
+ * @h : the head
+ * Return: number of element
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *curr = malloc(sizeof(dlistint_t));
