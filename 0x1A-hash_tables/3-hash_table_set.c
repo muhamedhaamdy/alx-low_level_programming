e1d8d4c38ddf0607cac90989066d9d119c35d0fe
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-30 20:47:12 +0200

    "hotfix"

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_ht->next = NULL;
 	if (!index)
 		return (0);
-	if (!ht->array[index])		
+	if (!ht->array[index])
 		ht->array[index] = new_ht;
 	else
 	{
