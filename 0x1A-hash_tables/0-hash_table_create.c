70de24266cf6d7d32f83f4c6250512e663536f5b
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-30 18:28:35 +0200

    "hotfix"

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_ht = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
 
+	if (!new_ht)
+		return (NULL);
 	new_ht->size = size;
-	new_ht->array = malloc(sizeof(hash_node_t) * size);
+	new_ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_ht->array)
+		return (NULL);
 	for (; i < size; i++)
 		new_ht->array[i] = NULL;
 	return (new_ht);
