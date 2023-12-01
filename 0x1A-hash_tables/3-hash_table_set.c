8a5197fac3ef612dcfa6315e3ab5c5532034a9c9
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-01 14:54:06 +0200

    "hotfix"

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,20 +35,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_node)
 		return (0);
 	new_node->key = strdup(key);
-	if (!new_node->key)
-	{
-		free(new_node);
-		return (0);
-	}
 	new_node->value = strdup(value);
-	if (!new_node->value)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
+	new_node->next = NULL;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-	new_node->next = NULL;
 	return (1);
 }
