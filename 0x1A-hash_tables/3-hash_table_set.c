7ba06f478fb8e33cdebe9f42775071fa68c6ead8
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-01 14:24:02 +0200

    "hotfix"

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,49 +1,51 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - set a key and a value on a hash hash_table_set
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table to add or update key/value to
+ * @key: key to add or update
+ * @value: value associated with key
  *
- * @ht : the hash table
- * @key : the key
- * @value : the value
- * Return: 1 or 0
-*/
+ * Return: 1 if successful, 0 if not
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int index = key_index((unsigned char *) key, ht->size);
-	hash_node_t *new_ht, *curr;
+	unsigned long int index;
+	hash_node_t *new_node = NULL, *temp = NULL;
 
-	if (!value || !ht)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	new_ht = malloc(sizeof(hash_node_t));
-	if (!new_ht)
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 		return (0);
-	new_ht->key = strdup(key);
-	if (!new_ht->key)
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
 	{
-		free(new_ht);
+		free(new_node);
 		return (0);
 	}
-	new_ht->value = strdup(value);
-	if (!new_ht->value)
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
 	{
-		free(new_ht->key);
-		free(new_ht);
+		free(new_node->key);
+		free(new_node);
 		return (0);
 	}
-	new_ht->next = NULL;
-	curr = ht->array[index];
-	if (curr)
+	new_node->next = NULL;
+	temp = ht->array[index];
+	if (temp != NULL)
 	{
-		new_ht->next = curr;
-		ht->array[index] = new_ht;
+		new_node->next = temp;
+		ht->array[index] = new_node;
 		return (1);
 	}
-	else 
+	else
 	{
-		ht->array[index] = new_ht;
+		ht->array[index] = new_node;
 		return (1);
 	}
+
 	return (0);
 }
