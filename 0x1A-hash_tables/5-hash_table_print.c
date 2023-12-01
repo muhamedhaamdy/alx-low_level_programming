6e0873d084484ce7705ceab8d29267a55584682b
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-01 19:52:00 +0200

    "hotfix"

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the whole hash table
+ *
+ * @ht - the hash table
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *curr;
+	long unsigned int i = 0, len;
+	char *buff = malloc(1024);
+
+	for (; i < 1024; i++)
+	{
+		buff[i] = '\0';
+	}
+	strcat(buff, "{");
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr)
+		{
+			if (curr->key || curr->value)
+			{
+				strcat(buff, "'");
+				strcat(buff, curr->key);
+				strcat(buff, "': '");
+				strcat(buff, curr->value);	
+				strcat(buff, "'");
+				strcat(buff, ", ");
+			}
+			curr = curr->next;
+		}
+	}
+	strcat(buff, "}\n");
+	len = strlen(buff);
+	for (i = 0; i < len; i++)
+	{
+		if((i == len - 4 || i == len - 3 ) && len > 3)
+			continue;
+		printf("%c", buff[i]);
+	}
+}
