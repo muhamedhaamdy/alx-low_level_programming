321f4fbdeb2770d47c0644408ecb2eb22d256a29
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-01-25 04:45:28 +0200

    "hotfix"

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,23 @@
 def island_perimeter(grid):
     '''function to claculate the perimeter of the island'''
     perimeter = 0
+    four = 4
     for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
             if grid[i][j]:
-                if j+1 < len(grid[i])  and not grid[i][j+1]:
-                    perimeter += 1
-                if j-1 >= 0 and  not grid[i][j-1]:
-                    perimeter += 1
-                if i+1 < len(grid) and not grid[i+1][j]:
-                    perimeter += 1
-                if i-1 >= 0  and not grid[i-1][j]:
-                    perimeter += 1
+                if j+1 < len(grid[i]) and grid[i][j+1]:
+                    four  -= 1
+                    perimeter += four
+                four = 4
+                if j-1 >= 0 and grid[i][j-1]:
+                    four  -= 1
+                    perimeter += four
+                four = 4
+                if i+1 < len(grid) and grid[i+1][j]:
+                    four  -= 1
+                    perimeter += four
+                four = 4
+                if i-1 >= 0  and grid[i-1][j]:
+                    four  -= 1
+                    perimeter += four
     return perimeter
