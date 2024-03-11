47de6bc8c300133ba12577dc9c3295e626eb677d
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-11 23:43:09 +0200

    "hotfix"

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,17 +11,17 @@
 
 void print_array(int *array, size_t l, size_t r)
 {
-    size_t i;
+	size_t i;
 
-    printf("Searching in array: ");
-    for (i = l; i <= r; i++)
-    {
-        printf("%d", array[i]);
-        if (i != r)
-            printf(", ");
-    }
-    printf("\n");
-	
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
+	{
+		printf("%d", array[i]);
+		if (i != r)
+			printf(", ");
+	}
+	printf("\n");
+
 }
 
 /**
@@ -39,17 +39,17 @@ int binary_search(int *array, size_t size, int value)
 	size_t l = 0, r = size - 1, mid;
 
 	if (!array)
-		return (-1);
+	return (-1);
 	while (l <= r)
 	{
 		mid = (l + r) / 2;
-		print_array(array, l , r);
+		print_array(array, l, r);
 		if (array[mid] == value)
 			return (mid);
-		else if (array[mid] > value)
+			else if (array[mid] > value)
 			r = mid - 1;
-		else
-			l = mid + 1;
+			else
+		l = mid + 1;
 	}
 	return (-1);
 }
